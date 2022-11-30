5ca94813729e4005426ec85ce6629aaafcf7d1d8
Kristaps-M <kristapsmitins@gmail.com>
2022-11-30 16:16:57 +0200

    update read me file

--- HierarchicalStructure/Program.cs
+++ HierarchicalStructure/Program.cs
@@ -6,17 +6,17 @@ tree.Root = new Branch(1);
 tree.Root.Left = new Branch(2);
 tree.Root.Right = new Branch(3);
 tree.Root.Left.Left = new Branch(4);
-tree.Root.Right.Left = new Branch(5);
-tree.Root.Right.Center = new Branch(6);
-tree.Root.Right.Right = new Branch(7);
-tree.Root.Right.Left.Left = new Branch(8);
-tree.Root.Right.Center.Left = new Branch(9);
-tree.Root.Right.Center.Right = new Branch(10);
-tree.Root.Right.Center.Left.Center = new Branch(11);
-tree.Root.Right.Center.Left.Center.Right = new Branch(12);
-tree.Root.Right.Center.Left.Center.Center = new Branch(13);
-tree.Root.Right.Center.Left.Center.Left = new Branch(14);
-tree.Root.Right.Center.Left.Center.Left.Center = new Branch(15);
+//tree.Root.Right.Left = new Branch(5);
+//tree.Root.Right.Center = new Branch(6);
+//tree.Root.Right.Right = new Branch(7);
+//tree.Root.Right.Left.Left = new Branch(8);
+//tree.Root.Right.Center.Left = new Branch(9);
+//tree.Root.Right.Center.Right = new Branch(10);
+//tree.Root.Right.Center.Left.Center = new Branch(11);
+//tree.Root.Right.Center.Left.Center.Right = new Branch(12);
+//tree.Root.Right.Center.Left.Center.Center = new Branch(13);
+//tree.Root.Right.Center.Left.Center.Left = new Branch(14);
+//tree.Root.Right.Center.Left.Center.Left.Center = new Branch(15);
 
 Console.WriteLine($"Depth of tree is : {tree.CalculateMaxDepth(tree.Root)}");
 
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # SimpleBinaryTreeConsoleApp
-Hierarchical structure of binary trees
+Hierarchical structure of binary trees.
+
+An ASP.NET Core Console .NET 6 application which calculates the depth of provided structure using recursion.
+
+### To run your program:
+
+If you are new to .NET visit [microsoft.com](https://dotnet.microsoft.com/en-us/learn)
+
+1. You need to clone or download project from GitHub [project page](https://github.com/kristaps-m/SimpleBinaryTreeConsoleApp)
+2. You need the .NET Framework and an IDE (integrated development environment) to work with the C# language. [Try this page to get started.](https://www.simplilearn.com/c-sharp-programming-for-beginners-article)
+3. Open and run project with capable IDE of your choice. I used Visual Studio in example below.
+
+<img src="pictures/run.png">
+
+4. In picture below we can see that depth is 1.
+
+<img src="pictures/deep_1.png">
+
+5. In picture below we can see that depth is 3. Fallowing green line we can see that depth is max depth of tree is 3.
+
+<img src="pictures/deep_3.png">
+
+6. Feel free to modify the tree and increase the depth. Comment out part of the code starting from bottom and run the aplication. And look how tree grows from one to much more complex.
